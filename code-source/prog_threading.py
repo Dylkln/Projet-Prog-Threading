43f2a4fe3b3abbe4ccc156cd5315046613de3491
Pierre-damase <pierre.damase.mbert@gmail.com>
2020-09-15 10:45:16 +0200

    Si dope-CA.par existe ne pas nettoyer  le fichier dope.par

--- code-source/prog_threading.py
+++ code-source/prog_threading.py
@@ -11,6 +11,7 @@ Usage:
 
 import sys
 from __modules__.fasta_file import *
+from __modules__.nettoyage_dope_file import *
 
 
 def arguments():
@@ -19,7 +20,7 @@ def arguments():
       - Le fichier fasta de la protéine cible.
       - Le fichier pdb de la protéine modèle.
     """
-    if len(sys.argv) != 2: # != 3 quand on rajoutera le fichier pdb
+    if len(sys.argv) != 3:
         sys.exit('Veuillez renseigner 2 arguments\n'
                  '  - La protéine cible (.fasta)\n'
                  '  - La protéine modèle (.pdb)')
@@ -28,4 +29,6 @@ def arguments():
 
 if __name__ == "__main__":
     arguments()
+    nettoyage_dope_file()
+
     protein = read_fasta_file(sys.argv[1])
